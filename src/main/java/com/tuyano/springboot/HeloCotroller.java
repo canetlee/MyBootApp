35ca3e530a8fca27b40d522debbb0d6add1be467
canet <canetlee12@gmail.com>
2017-05-10 14:56:00 +0900

    코드 3-7

--- src/main/java/com/tuyano/springboot/HeloCotroller.java
+++ src/main/java/com/tuyano/springboot/HeloCotroller.java
@@ -1,13 +1,16 @@
 package com.tuyano.springboot;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HeloCotroller {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Hello Spring-Boot World!!";
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num) {
+		int res = 0;
+		for(int i =1;i <= 1;i++) res +=1;
+		return "total : "+ res;
 	}
 }
